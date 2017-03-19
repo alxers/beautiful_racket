7b72c83651adf95a8417e52bc22b311f9589132a
Alexey Ershov <alex4rom@gmail.com>
2017-03-19 22:52:34 +0300

    Close paren

--- stacker/stacker.rkt
+++ stacker/stacker.rkt
@@ -3,8 +3,8 @@
 (define (read-syntax path port)
   (define src-lines (port->lines port))
   (define src-datums (format-datum '(handle ~a) src-lines))
-  (define module-datum `(module stacker-mod br
+  (define module-datum `(module stacker-mod "stacker.rkt"
                           ,@src-datums))
-  (datum->syntax #f '(module-datum))
+  (datum->syntax #f '(module-datum)))
 
 (provide read-syntax)
