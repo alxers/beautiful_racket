2cb596279ac3b458a9de63303dd9f25e5e958579
Alexey Ershov <alex4rom@gmail.com>
2017-04-06 22:07:06 +0300

    Define macro for operators

--- stackerizer/stackerizer.rkt
+++ stackerizer/stackerizer.rkt
@@ -6,6 +6,10 @@
      (for-each displayln (reverse (flatten EXPR)))))
 (provide (rename-out [stackerizer-mb #%module-begin]))
 
-(define-macro-cases +
-  [(+ FIRST) #'FIRST]
-  [(+ FIRST NEXT ...) #'(list '+ FIRST (+ NEXT ...))])
+(define-macro (define-op OP)
+  '#(define-macro-cases OP
+      [(OP FIRST) #'FIRST]
+      [(OP FIRST NEXT (... ...)) #'(list 'OP FIRST (OP NEXT (... ...)))]))
+
+(define-op +)
+(define-op *)
