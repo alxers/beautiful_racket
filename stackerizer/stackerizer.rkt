ec34ff782b675193353ad5e9ed3b940788c3550b
Alexey Ershov <alex4rom@gmail.com>
2017-04-08 21:52:25 +0300

    Add todo

--- stackerizer/stackerizer.rkt
+++ stackerizer/stackerizer.rkt
@@ -6,6 +6,7 @@
      (for-each displayln (reverse (flatten EXPR)))))
 (provide (rename-out [stackerizer-mb #%module-begin]))
 
+;; TODO: define with ellipses
 (define-macro (define-op OP)
   '#(define-macro-cases OP
       [(OP FIRST) #'FIRST]
